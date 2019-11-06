4331adf61defb2cbf7a08ef6b196e02428141298
blackutau <teresafloresc@hotmail.com>
2019-11-06 00:19:53 -0500

    implementacion de CategoryService

--- src/main/java/com/service/CategoryService.java
+++ src/main/java/com/service/CategoryService.java
@@ -1,9 +1,12 @@
 package com.hampcode.service;
 
-import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import com.hampcode.model.entity.Category;
 
-public interface CategoryService extends CrudService<Category, Long>{
-	public List<Category> fetchCategoryByName (String name) throws Exception;
+public interface CategoryService extends CrudService<Category, Long>{	
+	public Page<Category> findAll(Pageable pageable) throws Exception;
+	public Page<Category> fetchByName(String name,Pageable pageable) throws Exception;
 }
